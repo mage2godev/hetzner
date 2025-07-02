929b9dae3875d68e927c8edabfef72b1c035efc2
mage2godev <73138194+mage2godev@users.noreply.github.com>
2025-07-02 10:16:24 +0300

    Update magento2.sh

--- magento2.sh
+++ magento2.sh
@@ -10,46 +10,8 @@ DB_PASS="magento_pass"
 MYSQL_ROOT_PASS="root_pass"
 ES_VERSION="7.17.22"
 
-# ------------------ Оновлення системи ------------------
-apt update && apt upgrade -y
-
-# ------------------ Встановлення базових пакунків ------------------
-apt install -y nginx mysql-server redis-server git curl unzip software-properties-common
-
-# ------------------ MySQL ------------------
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$MYSQL_ROOT_PASS'; FLUSH PRIVILEGES;"
-mysql -u root -p$MYSQL_ROOT_PASS -e "CREATE DATABASE $DB_NAME;"
-mysql -u root -p$MYSQL_ROOT_PASS -e "CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';"
-mysql -u root -p$MYSQL_ROOT_PASS -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost'; FLUSH PRIVILEGES;"
-
-# ------------------ PHP 8.2 ------------------
-add-apt-repository ppa:ondrej/php -y && apt update
-apt install -y php8.2 php8.2-cli php8.2-fpm php8.2-mysql php8.2-curl php8.2-xml php8.2-mbstring php8.2-bcmath php8.2-soap php8.2-intl php8.2-zip php8.2-gd php8.2-readline php8.2-opcache
-
-# ------------------ Composer 2.2 ------------------
-EXPECTED_VERSION="2.2.21"
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php composer-setup.php --version=$EXPECTED_VERSION --install-dir=/usr/local/bin --filename=composer
-rm composer-setup.php
-
-# ------------------ Elasticsearch ------------------
-curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | gpg --dearmor -o /etc/apt/trusted.gpg.d/elastic.gpg
-echo "deb https://artifacts.elastic.co/packages/$ES_VERSION/apt stable main" | tee /etc/apt/sources.list.d/elastic-$ES_VERSION.list
-apt update && apt install elasticsearch -y
-systemctl enable elasticsearch
-systemctl start elasticsearch
-
-# ------------------ Varnish ------------------
-apt install -y varnish
-
 # ------------------ Magento ------------------
-mkdir -p $MAGENTO_DIR
 cd $MAGENTO_DIR
-
-composer config --global http-basic.repo.magento.com c5c73502f26b15c3d929a481280c862f 06f86d7b8cc8e508fd2878af817b9ba5
-
-# Встановлення Magento через Composer
-composer create-project --repository=https://repo.magento.com/ magento/project-community-edition=$MAGENTO_VERSION .
 
 # Налаштування прав
 chown -R www-data:www-data $MAGENTO_DIR
